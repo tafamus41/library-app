63fb111329cc466e4937f25966fa3e93b1640e49
asimov <seyranmustafa4@gmail.com>
2024-09-07 18:25:58 +0300

    p

--- src/components/KitapForm.jsx
+++ src/components/KitapForm.jsx
@@ -4,29 +4,29 @@ import Form from "react-bootstrap/Form";
 function KitapForm() {
   return (
     <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
+      <Form.Group className="mb-3" controlId="kitapAdı">
         <Form.Label>Kitap Adı</Form.Label>
-        <Form.Control type="text" placeholder="Kitap Adı" />
+        <Form.Control type="text" placeholder="Kitap Adı" required/>
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicPassword">
+      <Form.Group className="mb-3" controlId="yazarAdı" >
         <Form.Label>Yazar Adı</Form.Label>
-        <Form.Control type="text" placeholder="Yazar Adı" />
+        <Form.Control type="text" placeholder="Yazar Adı" required />
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
+      <Form.Group className="mb-3" controlId="ISBN" >
         <Form.Label>ISBN</Form.Label>
-        <Form.Control type="TEXT" placeholder="ISBN" />
+        <Form.Control type="text" placeholder="ISBN" required/>
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicPassword">
+      <Form.Group className="mb-3" controlId="kapakResmi" >
         <Form.Label>Kapak Resmi</Form.Label>
-        <Form.Control type="text" placeholder="Kapak Resmi" />
+        <Form.Control type="text" placeholder="Kapak Resmi" required/>
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
+      <Form.Group className="mb-3" controlId="tür" >
         <Form.Label>Tür</Form.Label>
-        <Form.Control type="text" placeholder="Tür" />
+        <Form.Control type="text" placeholder="Tür" required/>
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicPassword">
+      <Form.Group className="mb-3" controlId="yayınlanmaTarihi" >
         <Form.Label>Yayınlanma Tarihi</Form.Label>
-        <Form.Control type="text" placeholder="Yayınlanma Tarihi" />
+        <Form.Control type="text" placeholder="Yayınlanma Tarihi" required />
       </Form.Group>
       <Button className="d-block mx-auto" variant="danger" type="submit">
         Submit
