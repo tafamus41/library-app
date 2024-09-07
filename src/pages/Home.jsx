40702fdad44c555e782a304f93a20ab01c8c6ca5
asimov <seyranmustafa4@gmail.com>
2024-09-07 19:23:01 +0300

    p

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,19 @@
 import { Row } from "react-bootstrap";
 import KitapForm from "../components/KitapForm";
+import { useEffect } from "react";
+import axios from "axios";
 
 const Home = () => {
+  const url = "https://clarus-library-api.vercel.app/books";
+
+  const getBilgiler = async () => {
+    const res = await axios.get(url);
+    console.log(res.data);
+  };
+useEffect(()=>{
+  getBilgiler();
+},[])
+
   return (
     <Row>
       <h1 className="text-center mt-4">TAFAMUS41 LİBRARY</h1>
