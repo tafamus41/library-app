35484d4b95ca5a8596dc5bbfe75da5cad0c1a134
asimov <seyranmustafa4@gmail.com>
2024-09-07 21:53:52 +0300

    p

--- src/components/KitapList.jsx
+++ src/components/KitapList.jsx
@@ -1,9 +1,54 @@
-
+import { Col } from "react-bootstrap";
+import { AiFillDelete } from "react-icons/ai";
+import { FaEdit } from "react-icons/fa";
 
 const KitapList = ({books}) => {
 console.log(books);
   return (
-    <div>KitapList</div>
+    <Col>
+<table className="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">ISBN</th>
+            <th scope="col">Kapak Resmi</th>
+            <th scope="col">Kitap Adı</th>
+            <th scope="col">Yazar Adı</th>
+            <th scope="col">Tür</th>
+            <th scope="col">Yayınlanma Yılı</th>
+            <th scope="col">Edit</th>
+          </tr>
+        </thead>
+        <tbody>
+          {books.map(({title,ISBN,image,genre,id,author,publicationYear }) => (
+            <tr key={id}>
+              <td>{ISBN}</td>
+              <td><img src={image} alt="" style={{ width: '100px', height:'100px'}}/></td>
+              <td>{title}</td>
+              <td>{author}</td>
+              <td>{genre}</td>
+              <td>{publicationYear}</td>
+              <td className="text-center ">
+                <AiFillDelete
+                  type="button"
+                  size={22}
+                  className="text-danger cursor-pointer"
+                //   onClick={() => deleteBilgi()}
+                />
+
+                <FaEdit
+                  data-bs-toggle="modal"
+                  data-bs-target="#editModal"
+                  size={20}
+                  type="button"
+                  className="me-2 text-warning cursor-pointer"
+                  onClick={() => ({  })}
+                />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Col>
   )
 }
 
